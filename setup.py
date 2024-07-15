e04c89671db92ccbd278e5c284657701b9ead65c
Rahul Rama Chandran <rahulrchandran00@gmail.com>
2024-07-15 23:42:38 +0100

    updated data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name                = "Interview Question Generator Project",
+    version             = '0.0.0',
+    author              = "Rahul Rama Chandran",
+    author_email        = 'rahulrchandran00@gmail.com',
+    packages            = find_packages(),
+    install_requires    = []
+)
